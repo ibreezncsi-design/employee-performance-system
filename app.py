d836407d3d2a4962a3f1f64fdc7e2b3bb6b90ebd
Ibreez Al-Aufi <ibreez@ncsi.gov.om>
2026-07-20 14:01:42 +0400

    Fix url_for import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 import os
 import psycopg2
 from psycopg2.extras import DictCursor
